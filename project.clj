93b947d5592272d60dcc66d7161054d70e81b219
julianka <julian.kittelson-aldred@wilder.org>
2015-09-12 16:08:30 -0500

    add garden and style.css

--- project.clj
+++ project.clj
@@ -2,10 +2,11 @@
   :description "FIXME: write description"
   :url "https://pure-tundra-5297.herokuapp.com/"
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [compojure "1.3.1"]
-                 [ring/ring-defaults "0.1.2"]
-                 [hiccup "1.0.5"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [compojure "1.4.0"]
+                 [garden "1.3.0-SNAPSHOT"]
+                 [hiccup "1.0.5"]
+                 [ring/ring-defaults "0.1.2"]]
   :plugins [[lein-ring "0.8.13"]]
   :ring {:handler chatter.handler/app}
   :profiles
--- src/chatter/handler.clj
+++ src/chatter/handler.clj
@@ -1,10 +1,11 @@
 (ns chatter.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
-            [ring.middleware.params :refer [wrap-params]]
+            [garden.core :refer [css]]
             [hiccup.page :as page]
-            [hiccup.form :as form]))
+            [hiccup.form :as form]
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [ring.middleware.params :refer [wrap-params]]))
 
 (declare message-form message-table)
 
@@ -14,11 +15,15 @@
   (page/html5
    [:head
     [:title "Chatter"]
-    (page/include-css "//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css")
+    (page/include-css "//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css"
+                      "/style.css")
     (page/include-js "//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js")]
    [:body
-    [:h1 "Our Chat App"]
+    [:h1 "My Chat App"]
     message-form
+    [:p "This is some "
+      [:span.special "special text"]
+      "!"]
     (message-table messages)]))
 
 (defn message-table
@@ -48,13 +53,20 @@
   [messages name new-message]
   (swap! messages conj {:name name :message new-message}))
 
+(def styles
+  (css [:h1 {:text-align "center", :color "purple"}]
+       [:p {:font-family "Georgia", :color "#333"}
+          [:.special {:font-weight "bold"}]]))
+
 (defroutes app-routes
   (GET "/" [] (generate-message-view @chat-messages))
+  (GET "/style.css" [] {:content-type "text/css" :body styles})
   (POST "/" {params :params}
         (let [name-param (get params "name")
               msg-param (get params "msg")
               new-messages (update-messages! chat-messages name-param msg-param)]
           (generate-message-view new-messages)))
+  (route/resources "/")
   (route/not-found "Not Found"))
 
 (def app (wrap-params app-routes))
